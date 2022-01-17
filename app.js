46e16309ef8373436709361c01d0d950c1435553
Procoder16 <smsoumik1608@gmail.com>
2022-01-18 02:06:38 +0530

    redirecting requests

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    const method = req.method;
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'dummy text');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     console.log(req.url, req.headers, req.method);
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello World!!!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(5000);
