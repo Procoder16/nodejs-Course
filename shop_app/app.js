9770554a5c7bf4f97037b68cebf14aa0a42fdcb0
Procoder16 <smsoumik1608@gmail.com>
2022-01-21 01:16:06 +0530

    understanding middlewares

--- shop_app/app.js
+++ shop_app/app.js
@@ -1,18 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('In the first middleware!');
-    next();
+
+app.use('/add-product',(req, res, next) => {
+    console.log('Add product middleware!');
+    res.send('<h1>Add Product Page</h1>');
 });
 
-app.use((req,res, next) => {
+app.use('/',(req,res, next) => {
     console.log('In the second middleware!');
     res.send('<h1>Hello from Soumik!!!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
