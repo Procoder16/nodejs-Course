40bc87ac0c3b79c58a533fb11716c04639b57470
Procoder16 <smsoumik1608@gmail.com>
2022-01-22 01:23:49 +0530

    installed bodyparser, handling parsing incoming body

--- shop_app/app.js
+++ shop_app/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use('/add-product',(req, res, next) => {
     console.log('Add product middleware!');
-    res.send('<h1>Add Product Page</h1>');
+    res.send('<form action="/product" method = "POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/',(req,res, next) => {
