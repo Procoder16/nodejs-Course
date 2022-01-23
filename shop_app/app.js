e352ff282ccb37063e824b7159ddff37eb6de139
Procoder16 <smsoumik1608@gmail.com>
2022-01-24 03:02:36 +0530

    made a separate file for the notes

--- shop_app/app.js
+++ shop_app/app.js
@@ -1,27 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const expressHbs = require('express-handlebars');  -> was used for handlebars
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
-
-// <--Way we initialise handlebars-->
-// Handlebars are not built in and hence we need to import its dependency
-// app.engine is used for creating an engine and 'handlebars' here is just a name we have given and initialised with expressHbs
-//layoutsDir and defaultLayout are parameters passed to configure the layout of the handlebars
-
-// app.engine('hbs', expressHbs({layoutsDir:'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}));
-// app.set('view engine', 'hbs');
-// app.set('views', 'views');
-
-// <-- Way we initialise pug engine -->
-// Pug is a built-in kind of an engine and hence we don't need to import it, we can directly use it
-
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
--- shop_app/routes/shop.js
+++ shop_app/routes/shop.js
@@ -4,23 +4,12 @@ const path = require('path');
 const router = express.Router();
 
 const adminData = require('./admin');
-/*
-    Firstly remember sendFile function needs the absolute location with respect to the OS
-    And hence we use path.join that joins all the paths given as parameter and we want to reach shop.html file
-    __dirname gives us the current directory we are in, in this case, we are in route
-    ../ means go up a level
-    then 'views' specifies the directory to look for
-    'shop.html' is the file we are looking for
-*/
 
 router.get('/', (req, res, next) => {
-    // console.log('shop.js', adminData.products);
-    // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    
     const products = adminData.products;
     res.render('shop', { prods: products, pageTitle: "Shop", path: '/', hasProducts: products.length > 0, activeShop:true, productCSS:true});
-    //here we don't need to mention the path as we have have already mentioned it while mentioning the views in app.js
-    //hasProducts parameter is passed to implement the handlebars to check whether we have any product or not
-    //make a note that we can also check {{#if prods}} and we no need to pass this extra argument
+    
 });
 
 module.exports = router;
