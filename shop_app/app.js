b99a11cd835d998cb8be30df2156aad969d1517c
Procoder16 <smsoumik1608@gmail.com>
2022-01-27 01:58:10 +0530

    enhanced the app

--- shop_app/app.js
+++ shop_app/app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -11,7 +10,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
