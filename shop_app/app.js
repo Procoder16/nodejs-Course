2a51605ea9b6f4f7ed5e878a3b92ebe2a0aa3377
Procoder16 <smsoumik1608@gmail.com>
2022-02-05 18:55:26 +0530

    learning mongodb

--- shop_app/app.js
+++ shop_app/app.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const mongoConnect = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,7 +27,12 @@ app.use((req, res, next) => {
   //   .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
