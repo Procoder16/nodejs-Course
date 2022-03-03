19dc22fead44402f98ac9336d1b6635e4d8c2068
Procoder16 <smsoumik1608@gmail.com>
2022-03-04 00:53:43 +0530

    mongodb module completed

--- shop_app/app.js
+++ shop_app/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
 const User = require('./models/user');
 
 const app = express();
@@ -22,11 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('6220f6449c473aa0eeae82c8')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);
