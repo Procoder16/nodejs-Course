863b93cf664eb51c805c5fe72f9214d7a438a0d5
Procoder16 <smsoumik1608@gmail.com>
2022-03-08 01:28:17 +0530

    setting up cookie

--- shop_app/routes/auth.js
+++ shop_app/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const router = express.Router;
+const authController = require('../controllers/auth');
 
-router.get('/login', (req, res, next) => {
-    res.render
-});
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
 
 module.exports = router;
