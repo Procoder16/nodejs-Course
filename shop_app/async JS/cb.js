1b610a5c37cf833a97691057429a6419d20d5573
Procoder16 <smsoumik1608@gmail.com>
2022-03-12 01:01:38 +0530

    advanced authentication section finished

--- shop_app/async JS/cb.js
+++ shop_app/async JS/cb.js
@@ -18,6 +18,7 @@
 // }
 //mongodb+srv://soumik:shopapp@cluster0.858cx.mongodb.net/shop?retryWrites=true&w=majority
 //622522ed17dd2de1ad858c7e
+// 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
 // function createPost(post, callback){
 //     setTimeout(() => {
 //         posts.push(post);
