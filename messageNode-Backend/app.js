4ab519963b82fa1e1dfc905d6226e98497bf1803
Procoder16 <smsoumik1608@gmail.com>
2022-04-03 12:44:42 +0530

    connected mongodb and defined the post schema

--- messageNode-Backend/app.js
+++ messageNode-Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,10 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(9080);
+mongoose.connect('mongodb+srv://soumik:shopapp@cluster0.858cx.mongodb.net/shop')
+.then(result => {
+    app.listen(9080);
+})
+.catch(err => {
+    console.log(err);
+});
--- messageNode-Backend/routes/feed.js
+++ messageNode-Backend/routes/feed.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -8,8 +8,12 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 router.post('/post', [
-    body('title').trim().isLength({ min: 5 }),
-    body('content').trim().isLength({ min:5 })
+    body('title')
+        .trim()
+        .isLength({ min: 5 }),
+    body('content')
+        .trim()
+        .isLength({ min:5 })
 ] ,feedController.createPost);
 
 module.exports = router;
