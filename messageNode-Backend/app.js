105335462341c48e62acbb930b42d21acb8ea5ad
Procoder16 <smsoumik1608@gmail.com>
2022-04-08 01:03:36 +0530

    implemented the user model and the auth route

--- messageNode-Backend/app.js
+++ messageNode-Backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -51,6 +52,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 // this is the error handling middleware
 app.use((error, req, res, next) => {
